aa221384ec378c3bc57c1fd34547b876697aa89d
veliohart <veliohart@gmail.com>
2017-09-05 23:47:00 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,22 @@ import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
+const User = {
+  template: '<div> user </div>'
+}
+
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/user/:id',
+      name: 'user',
+      component: User,
+      children: []
     }
   ]
 })
